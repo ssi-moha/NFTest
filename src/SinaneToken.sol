98393b318c43c5be1b5087733678c27463d8bcb6
Sinane <sin.sim@outlook.fr>
2023-04-16 19:59:51 +0200

    feat: presale function

--- src/SinaneToken.sol
+++ src/SinaneToken.sol
@@ -1,26 +1,61 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
 
 error MaxSupplyReached();
+error PresaleMaxSupplyReached();
+error PresaleClosed();
+error PresaleMintLimitReached(address);
+error InvalidAmount(address);
 
 contract SinaneToken is ERC721, Ownable {
     using Counters for Counters.Counter;
+    
+    bool activePresale = false;
+    uint256 timeFromPresaleDebut;
+    
+    uint256 public constant presalePrice = 0.003 ether;
+    uint256 public constant salePrice = 0.005 ether;
 
     uint256 public constant MAX_SUPPLY = 300;
+    uint256 public constant MAX_PRESALE_SUPPLY = 300;
+    mapping(address => uint256) public mintCounter;
 
     Counters.Counter private _tokenIdCounter;
+    Counters.Counter private _presaleCounter;
 
     constructor() ERC721("SinaneToken", "SIN") {}
 
-    function safeMint(address to) public onlyOwner {
+    function setActivePresale () external onlyOwner {
+        activePresale = true;
+        timeFromPresaleDebut = block.timestamp;
+    }
+
+    function isPresaleExpired() public view returns (bool) {
+        return block.timestamp - timeFromPresaleDebut > 1 days;
+    }
+
+    function presale() payable external {
+        address to = msg.sender;
+        if(!activePresale || isPresaleExpired()) revert PresaleClosed();
+        if(msg.value != presalePrice) revert InvalidAmount(to);
+        if(mintCounter[to] >= 2) revert PresaleMintLimitReached(to);
+        if(_tokenIdCounter.current() == 100) revert PresaleMaxSupplyReached();
+
+        _presaleCounter.increment();
+        safeMint(to);
+    }
+
+    function safeMint(address to) internal {
         uint256 tokenId = _tokenIdCounter.current();
 
         if (tokenId >= MAX_SUPPLY) revert MaxSupplyReached();
 
         _tokenIdCounter.increment();
+        mintCounter[to]++;
         _safeMint(to, tokenId);
     }
 }
--- test/SinaneTokenTest.t.sol
+++ test/SinaneTokenTest.t.sol
@@ -3,17 +3,84 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/SinaneToken.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract SinaneTokenTest is Test {
+contract SinaneTokenTest is Test, IERC721Receiver {
     SinaneToken token;
 
-    function setUp () public {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+
+    function setUp() public {
         token = new SinaneToken();
     }
 
-    function reachMaxSupply () public {
-        for (uint256 supply = 0; supply <= 300; supply++) {
-            token.safeMint(address(this));
+    // function testReachMaxSupply() public {
+    //     for (uint256 supply = 0; supply < 300; supply++) {
+    //         token.safeMint(address(this));
+    //     }
+
+    //     vm.expectRevert(MaxSupplyReached.selector);
+    //     token.safeMint(address(this));
+    // }
+
+    function testPresaleReachMintLimit() public {
+        token.setActivePresale();
+        token.presale{value: 0.003 ether}();
+        token.presale{value: 0.003 ether}();
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                PresaleMintLimitReached.selector,
+                address(this)
+            )
+        );
+        token.presale{value: 0.003 ether}();
+    }
+
+    function testPresaleInvalidAmount() public {
+        token.setActivePresale();
+
+        vm.expectRevert(
+            abi.encodeWithSelector(InvalidAmount.selector, address(this))
+        );
+        token.presale{value: 0.001 ether}();
+    }
+
+    function testPresaleClosed() public {
+        vm.expectRevert(PresaleClosed.selector);
+        token.presale();
+    }
+
+    function testPresaleExpired() public {
+        token.setActivePresale();
+        vm.warp(block.timestamp + 1 days + 1 seconds);
+        
+        vm.expectRevert(PresaleClosed.selector);
+        token.presale();
+    }
+
+    function testPresaleMaxSupplyReached() public {
+        token.setActivePresale();
+
+        for (uint256 supply = 0; supply < 100; supply++) {
+            hoax(vm.addr(supply + 1));
+            token.presale{value: 0.003 ether}();
         }
+
+        vm.expectRevert(PresaleMaxSupplyReached.selector);
+        token.presale{value: 0.003 ether}();
+    }
+
+    function testPresaleSuccess() public {
+        token.setActivePresale();
+        token.presale{value: 0.003 ether}();
+        token.presale{value: 0.003 ether}();
     }
 }
